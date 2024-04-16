b1e4255c5d97675fa37a8d48339d905887393da0
fetol <bilka.miso@gmail.com>
2024-04-16 23:02:11 +0200

    first commit

---
+++ projekt_programko/src/projekt_programko/App.java
@@ -0,0 +1,10 @@
+package projekt_programko;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ projekt_programko/src/projekt_programko/Databaze.java
@@ -0,0 +1,14 @@
+package projekt_programko;
+
+public class Databaze {
+
+	public static void main(String[] args) {
+		mamvelkypenis();
+	}
+
+	private static void mamvelkypenis() {
+	
+		
+	}
+
+}
