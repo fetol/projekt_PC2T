51685204243d6062648d1009ba1f89dd71841d8f
HanaO10 <114721175+HanaO10@users.noreply.github.com>
2024-04-29 23:41:38 +0200

    Update App.java

--- projekt_programko/src/projekt_programko/App.java
+++ projekt_programko/src/projekt_programko/App.java
@@ -1,12 +1,25 @@
 package projekt_programko;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		knihovna knihovna = new knihovna();
 
+        // Přidání nové knihy
+        String title = "Introduction to Java";
+        List<String> authors = Arrays.asList("John Doe");
+       
+        int year = 2022;
+        
+        String type = "ucebnice"; // Typ knihy
+        knihovna.addKnihu(title, authors, year, type);
+        
+        
+
+        
+        knihovna.listovaniKnih();
 	}
-
 }
-
-		//DODELAT volby a vyjimky
