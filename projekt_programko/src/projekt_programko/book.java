e2f6ab703eed5e7abb9553c187ec39e29be8a60f
olive <olive@147.229.193.49>
2024-04-29 18:25:26 +0200

    Třída book.java by měla být dodělána i s konstruktorem book a s getery a setery. Nejsem si jistý podmínkou na řádku 37.

--- projekt_programko/src/projekt_programko/book.java
+++ projekt_programko/src/projekt_programko/book.java
@@ -1,1 +1,43 @@
-
+package projekt_programko;
+import java.util.List;
+public class book {
+	private String nazev;
+	private List <String> autor;
+	private int rok_vydani;
+	private boolean stav_vypujcky;
+	
+	public book (String nazev, List <String> autor, int rok_vydani) {
+		this.nazev= nazev;
+		this.autor= autor;
+		this.rok_vydani = rok_vydani;
+		this.stav_vypujcky = false
+	}
+	public String getNazev() {
+		return nazev;
+	}
+	public void setNazev(String nazev) {
+		this.nazev = nazev;
+	}
+	public List<String> getAutor(){
+		return autor;
+	}
+	public void setAutor(List<String>autor) {
+		this.autor = autor;
+	}
+	public int getRok_vydani() {
+		return rok_vydani;
+	}
+	public int setRok_vydaní() {
+		this.rok_vydani = rok_vydani;
+	}
+	public boolean stav_vypujcky() {
+		return stav_vypujcky;
+	}
+	public void set stav_vypujcky(boolean pujceno) {
+		if(!pujceno) {
+			this.stav_vypujcky = pujceno;
+		}
+		
+			
+	}
+}
